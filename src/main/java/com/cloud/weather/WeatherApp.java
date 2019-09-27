6719d4e746159a8d2f5ba2d0763f3cea3f1dfad5
DESKTOP-C18PLRG\Xsam <sz.sam@foxmail.com>
2019-09-28 02:15:33 +0800

    initial project file

---
+++ src/main/java/com/cloud/weather/WeatherApp.java
@@ -0,0 +1,11 @@
+package com.cloud.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApp {
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherApp.class);
+    }
+}
