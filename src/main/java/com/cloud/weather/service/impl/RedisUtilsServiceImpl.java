03a9b86b66d24257f76af05aa747db01d7ee3737
DESKTOP-C18PLRG\Xsam <sz.sam@foxmail.com>
2019-09-29 11:28:18 +0800

    Add support for Redis

---
+++ src/main/java/com/cloud/weather/service/impl/RedisUtilsServiceImpl.java
@@ -0,0 +1,154 @@
+package com.cloud.weather.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.*;
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+import java.util.List;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class RedisUtilsServiceImpl {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    // String data write to cache
+    public boolean set(final String key, Object value) {
+        boolean result = false;
+        try {
+            ValueOperations<Serializable, Object> operations = redisTemplate.opsForValue();
+            operations.set(key, value);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    // String data write to cache with expire time
+    public boolean set(final String key, Object value, Long expireTime, TimeUnit timeUnit) {
+        boolean result = false;
+        try {
+            ValueOperations<Serializable, Object> operations = redisTemplate.opsForValue();
+            operations.set(key, value);
+            redisTemplate.expire(key, expireTime, timeUnit);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    // To set expire time
+    public boolean expire(String key, long time, TimeUnit timeUnit) {
+        try {
+            if (time > 0) {
+                redisTemplate.expire(key, time, timeUnit);
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    //
+    public void remove(final String... keys) {
+        for (String key : keys) {
+            remove(key);
+        }
+    }
+
+    public void removePattern(final String pattern) {
+        Set<Serializable> keys = redisTemplate.keys(pattern);
+        if (keys.size() > 0) {
+            redisTemplate.delete(keys);
+        }
+    }
+
+    public void remove(final String key) {
+        if (exists(key)) {
+            redisTemplate.delete(key);
+        }
+    }
+
+    public boolean exists(final String key) {
+        return redisTemplate.hasKey(key);
+    }
+
+    public Object get(final String key) {
+        Object result = null;
+        ValueOperations<Serializable, Object> operations = redisTemplate.opsForValue();
+        result = operations.get(key);
+        return result;
+    }
+
+
+    public void hmSet(String key, Object hashKey, Object value) {
+        HashOperations<String, Object, Object> hash = redisTemplate.opsForHash();
+        hash.put(key, hashKey, value);
+    }
+
+    public Object hmGet(String key, Object hashKey) {
+        HashOperations<String, Object, Object> hash = redisTemplate.opsForHash();
+        return hash.get(key, hashKey);
+    }
+
+    public Map<Object, Object> hmget(String key) {
+        return redisTemplate.opsForHash().entries(key);
+    }
+
+    public boolean hmset(String key, Map<String, Object> map) {
+        try {
+            redisTemplate.opsForHash().putAll(key, map);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void lPush(String k, Object v) {
+        ListOperations<String, Object> list = redisTemplate.opsForList();
+        list.rightPush(k, v);
+    }
+
+    public Long lSize(String k) {
+        ListOperations<String, Object> list = redisTemplate.opsForList();
+        return list.size(k);
+    }
+
+    public List<Object> lRange(String k, long l, long l1) {
+        ListOperations<String, Object> list = redisTemplate.opsForList();
+        return list.range(k, l, l1);
+    }
+
+    public void add(String key, Object value) {
+        SetOperations<String, Object> set = redisTemplate.opsForSet();
+        set.add(key, value);
+    }
+
+    public Set<Object> setMembers(String key) {
+        SetOperations<String, Object> set = redisTemplate.opsForSet();
+        return set.members(key);
+    }
+
+    public void zAdd(String key, Object value, double scoure) {
+        ZSetOperations<String, Object> zset = redisTemplate.opsForZSet();
+        zset.add(key, value, scoure);
+    }
+
+    public Set<Object> rangeByScore(String key, double scoure, double scoure1) {
+        ZSetOperations<String, Object> zset = redisTemplate.opsForZSet();
+        return zset.rangeByScore(key, scoure, scoure1);
+    }
+
+
+
+
+}
--- src/main/java/com/cloud/weather/service/impl/WeatherDataServiceImpl.java
+++ src/main/java/com/cloud/weather/service/impl/WeatherDataServiceImpl.java
@@ -5,19 +5,25 @@ import com.cloud.weather.vo.Weather;
 import com.cloud.weather.vo.WeatherResponse;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class WeatherDataServiceImpl implements WeatherDataService {
 
     private final static String WEATHER_URI = "https://www.tianqiapi.com/api/?version=v1&appid=36277646&appsecret=Im66r7zN&";
-
+    private final static Logger logger = LoggerFactory.getLogger(WeatherDataServiceImpl.class);
     @Autowired
     private RestTemplate restTemplate; //只有Spring web环境才好用
+    @Autowired
+    private RedisUtilsServiceImpl redisUtilsService;
+
     @Override
     public WeatherResponse getDataByCityId(String cityId) {
         String uri = WEATHER_URI + "cityid=" + cityId;
@@ -31,12 +37,20 @@ public class WeatherDataServiceImpl implements WeatherDataService {
     }
 
     private WeatherResponse  doGetWeather(String uri){
+
+        String key = uri;
+        // 先查缓存，如果缓存有直接取缓存中数据
+        //如果缓存没有，则去调用第三方接口
+        if(redisUtilsService.get(key) != null){
+            logger.info("缓存命中");
+            return (WeatherResponse)redisUtilsService.get(key);
+        }
+        logger.info("缓存没有命中");
         WeatherResponse weatherResponse = null;
 //      WeatherResponse weatherResponse1 = restTemplate.getForEntity(uri,WeatherResponse.class).getBody();
         ResponseEntity<String> respStr =  restTemplate.getForEntity(uri,String.class);
         // json字符串转Object
         ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
         String strBody = null;
         if(respStr.getStatusCodeValue() == 200){
             strBody =  respStr.getBody();
@@ -48,14 +62,14 @@ public class WeatherDataServiceImpl implements WeatherDataService {
                 weatherResponse.setData(weather);
                 weatherResponse.setStatus(200);
                 weatherResponse.setDesc("成功");
+                redisUtilsService.set(key,weatherResponse,1L,TimeUnit.HOURS);
             }else{
                 weatherResponse.setStatus(500);
                 weatherResponse.setDesc("失败");
             }
 //            weatherResponse = objectMapper.readValue(strBody,WeatherResponse.class);
         } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println(e.getMessage());
+            logger.error(e.getMessage());
         }
         return weatherResponse;
 
